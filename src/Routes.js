c731fdea99e43603c2af353e6f4441fc9bf98366
Humna Aleem <Humna@rebeltechnology.io>
2021-06-08 16:30:18 +0500

    JO-4 create separate route for time selector

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import { Switch, Route } from 'react-router-dom';
-// import { Home } from './components/home/Home'
+import { Home } from './components/home/Home'
 import { TimeSelector } from './components/time-selector/TimeSelector'
 
 const Routes = () => {
 
   return (
         <Switch>
-            <Route path="/" component={TimeSelector} />
+            <Route path="/" component={Home} />
+            <Route path="/time-selector" component={TimeSelector} />
         </Switch>
     )
 }
