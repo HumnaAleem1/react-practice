44e56aa5535342e6d3f137a4857acb6697ee6cba
Humna Aleem <Humna@rebeltechnology.io>
2021-06-18 12:52:47 +0500

    existance of child component in Home

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,11 +1,25 @@
 import { Home } from './Home'
-import { shallow } from 'enzyme';
+import { Table } from '../Table/Table'
+import { shallow, render, mount } from 'enzyme';
+const home = jest.mock('./Home');
 
-test('should contain this text', () => {
+describe('Home', () => {
 
-    const wrapper = shallow(
-        <Home />
-    )
+    test('should contain this text', () => {
+        const wrapper = shallow(<Home />)
+        expect(wrapper.text()).toMatch('Test App')
+    })
 
-    expect(wrapper.text()).toMatch('Test App')
-})
+    test('should call getRedditData function', () => {
+        const getRedditdata = jest.fn();
+        const wrapper = shallow(<Home onClick={getRedditdata} />)
+        wrapper.find('button').simulate('click');
+        expect(wrapper.find('button').length).toEqual(1);
+    })
+
+    test('should render Table component(child)', () => {
+        const wrapper = mount(<Home />)
+        expect(wrapper.find('Home').children().length).toEqual(1);
+    })
+
+});
