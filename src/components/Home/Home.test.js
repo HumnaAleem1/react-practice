e6628340b5929bbb66fc4ef0ab6affcedb8afb8a
Humna Aleem <Humna@rebeltechnology.io>
2021-06-18 16:22:00 +0500

    JO-17 test setstate

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Home } from './Home'
 import { shallow, render, mount } from 'enzyme';
 const home = jest.mock('./Home');
@@ -12,18 +13,27 @@ describe('Home', () => {
     test('should call getRedditData function', () => {
         const getRedditdata = jest.fn();
         const wrapper = shallow(<Home />)
-        wrapper.find('button').simulate('click');
-        expect(wrapper.find('button').length).toEqual(1);
+        wrapper.find('button').simulate('click')
+        expect(wrapper.find('button').length).toEqual(1)
     })
 
     test('should render Table component(child)', () => {
         const wrapper = mount(<Home />)
-        expect(wrapper.children().exists()).toEqual(true);
+        expect(wrapper.children().exists()).toEqual(true)
     })
 
     test('Should return count of child elements', () => {
         const wrapper = mount(<Home />)
         expect(wrapper.children().length).toEqual(1)
+    })
+
+    test('Should set state of authorData', () => {
+        const setAuthorData = jest.fn()
+        const wrapper = shallow(<Home />)
+        const handleClick = jest.spyOn(React, "useState")
+        handleClick.mockImplementation(authorData => [authorData, setAuthorData])
+        wrapper.find('button').simulate('click')
+        expect(setAuthorData).toBeTruthy()
     })
 
 });
